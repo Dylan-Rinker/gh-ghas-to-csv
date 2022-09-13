93fc0163ba23be3c1dbdae45695acada0f2b904f
Dylan Rinker <dylan-rinker@github.com>
2022-09-13 15:38:56 -0400

    add cmd

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/cli/go-gh"
+	"github.com/dylan-rink/gh-ghas-to-csv/cmd"
 )
 
 func main() {
+
+	// Instantiate and execute root command
+	cmd := cmd.NewCmd()
+
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+
 	fmt.Println("hi world, this is the gh-ghas-to-csv extension!")
 	client, err := gh.RESTClient(nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	response := struct {Login string}{}
+	response := struct{ Login string }{}
 	err = client.Get("user", &response)
 	if err != nil {
 		fmt.Println(err)
