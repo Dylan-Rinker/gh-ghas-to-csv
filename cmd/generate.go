2143a9b4a2700e31ceff63f42ab2d5fcc6a352de
Dylan Rinker <dylan-rinker@github.com>
2022-09-13 23:36:35 +0000

    changes

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,17 +1,84 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
-func NewCmd() *cobra.Command {
+// type cmdFlags struct {
+// 	enterprise   string
+// 	organization string
+// 	repository   string
+// }
 
-	// Instantiate struct to contain values from cobra flags; arguments are handled within RunE
-	// cmdFlags := cmdFlags{}
+var enterprise string
+var organization string
+var repository string
 
-	// Instantiate cobra command driving work from package
-	// Closures are used for cobra command lifecycle hooks for access to cobra flags struct
-	cmd := cobra.Command{}
+// generateCmd represents the generate command
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
 
-	return &cmd
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		
+		if len(args) == 0 {
+			runInteractiveMode()
+		}
+
+		// var err error
+		// var client api.GQLClient
+
+		// client, err = gh.GQLClient(&api.ClientOptions{
+		// 	Headers: map[string]string{
+		// 		"Accept": "application/vnd.github.hawkgirl-preview+json",
+		// 	},
+		// })
+
+		// if err != nil {
+		// 	return err
+		// }
+
+	},
 }
+
+func init() {
+	rootCmd.AddCommand(generateCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// generateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// generateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	generateCmd.Flags().StringVarP(&enterprise, "enterprise", "e", "", "Generate GHAS CSV for an enterprise.")
+	generateCmd.Flags().StringVarP(&organization, "organization", "o", "", "Generate GHAS CSV for an organization.")
+	generateCmd.Flags().StringVarP(&repository, "repository", "r", "", "Generate GHAS CSV for an repository.")
+	// generateCmd.MarkFlagsRequiredTogether("repository", "organization") // To Do: Repo requires org, but org doesn't require repo
+}
+
+// A function that prints the word hello
+func runInteractiveMode() {
+	fmt.Println("Running in interactive mode")
+}
+
+// func runEmpty() {
+// 	fmt.Println('Running Empty')
+// }
+
+// A function that checks is a variable is an empty array {
+// func (a []string) IsEmpty() bool {
+// 	return len(a) == 0
+// }
--- main.go
+++ main.go
@@ -1,36 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/cli/go-gh"
-	"github.com/dylan-rinker/gh-ghas-to-csv/cmd"
-)
+import "github.com/Dylan-Rinker/gh-ghas-to-csv/cmd"
 
 func main() {
-
-	// Instantiate and execute root command
-	cmd := cmd.NewCmd()
-
-	if err := cmd.Execute(); err != nil {
-		os.Exit(1)
-	}
-
-	fmt.Println("hi world, this is the gh-ghas-to-csv extension!")
-	client, err := gh.RESTClient(nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	response := struct{ Login string }{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
+	cmd.Execute()
 }
-
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
